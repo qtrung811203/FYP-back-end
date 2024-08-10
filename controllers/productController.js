0038db5640e161c9c4c84734c696a90d0263f3ed
Trung <qtrung811203@gmail.com>
2024-08-10 12:02:39 +0700

    Add Error Catch => throw error when user CRUD product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
   // Build query
@@ -35,6 +36,9 @@ exports.createProduct = catchAsync(async (req, res, next) => {
 
 exports.getProduct = catchAsync(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -48,6 +52,9 @@ exports.updateProduct = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -58,6 +65,9 @@ exports.updateProduct = catchAsync(async (req, res, next) => {
 
 exports.deleteProduct = catchAsync(async (req, res, next) => {
   const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
