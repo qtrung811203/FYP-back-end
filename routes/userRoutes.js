1413f352bd6d5500e2df50c47124e1b75433fc38
Trung <qtrung811203@gmail.com>
2024-08-11 22:37:19 +0700

    user - update password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,8 @@ router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
+router
+  .route('/updatePassword')
+  .patch(authController.protect, authController.updatePassword);
+
 exports.router = router;
