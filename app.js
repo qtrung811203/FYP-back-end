bb5650bb80c9758e1f890655c47c406a2000c241
Trung <qtrung811203@gmail.com>
2024-08-12 14:35:34 +0700

    add rate limit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
+
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const app = express();
 
 //Error handling
 const AppError = require('./utils/appError');
@@ -11,13 +14,21 @@ const globalErrorHandler = require('./controllers/errorController');
 
 dotenv.config({ path: './config.env' });
 
-//All the middleware put here
+//MIDDLEWARES
 app.use(express.json());
 
 //HTTP request logger
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 //Routes
 app.use('/api/v1/products', productRouter.router);
