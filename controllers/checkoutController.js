eb7e3643396417945d2dfcc41b885748fe32f0d9
Trung <qtrung811203@gmail.com>
2024-11-04 23:33:22 +0700

    chore: Update checkout routes and controller for Stripe integration

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -2,28 +2,55 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { meta } = require('eslint-plugin-prettier');
 
+//Tạo checkout session và trả về session id
 exports.createCheckoutSession = catchAsync(async (req, res, next) => {
+  const { user, items } = req.body;
   const hostUrl = req.headers.origin;
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `${hostUrl}/sucess/{CHECKOUT_SESSION_ID}`,
-    cancel_url: `${hostUrl}/cancel`,
-    line_items: req.body.items.map((item) => {
-      return {
-        price_data: {
-          currency: 'vnd',
-          product_data: {
-            name: item.name,
-            images: [item.imageItem],
-          },
-          unit_amount: item.price,
+    success_url: `${hostUrl}/checkout/success/{CHECKOUT_SESSION_ID}`,
+    cancel_url: `${hostUrl}/checkout/cancel/{CHECKOUT_SESSION_ID}`,
+    customer_email: user.email,
+    metadata: {
+      email: user.email,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+      address: user.address,
+      province: user.province,
+      district: user.district,
+      ward: user.ward,
+    },
+    line_items: items.map((item) => ({
+      price_data: {
+        currency: 'vnd',
+        product_data: {
+          name: item.name,
+          images: [item.imageItem],
         },
-        quantity: item.quantity,
-      };
-    }),
+        unit_amount: item.price,
+      },
+      quantity: item.quantity,
+    })),
   });
+
+  res.status(200).json({
+    status: 'success',
+    sessionId: session.id,
+  });
+});
+
+//Lấy thông tin của checkout session
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const { sessionId } = req.params;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+  if (!session) {
+    return next(new AppError('No session found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -31,7 +58,8 @@ exports.createCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+//Unfinished
+exports.checkSession = catchAsync(async (req, res, next) => {
   const { sessionId } = req.params;
   const session = await stripe.checkout.sessions.retrieve(sessionId);
 
--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -3,10 +3,17 @@ const checkoutController = require('../controllers/checkoutController');
 
 const router = express.Router();
 
+//Create a checkout session
 router
   .route('/create-checkout-session')
   .post(checkoutController.createCheckoutSession);
 
+//Check session status
+router
+  .route('/check-session-status/:sessionId')
+  .get(checkoutController.checkSession);
+
+//Get checkout session
 router.route('/session/:sessionId').get(checkoutController.getCheckoutSession);
 
 module.exports = router;
